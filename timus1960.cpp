280b8ebc08d6a59756a36c6db748a96cae2e8dc0
Dmitry Gozman <dgozman@gmail.com>
2026-01-11 14:59:14 +0000

    1958, 1960

---
+++ timus1960.cpp
@@ -0,0 +1,183 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cstring>
+#include <cmath>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <string>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+bool __hack1 = std::ios::sync_with_stdio(false);
+auto __hack2 = cin.tie(nullptr);
+template<typename T> using vec = vector<T>;
+template<typename T> using vec2d = vec<vec<T>>;
+template<typename T> using vec3d = vec<vec2d<T>>;
+using i64 = int64_t;
+using u64 = uint64_t;
+using i32 = int32_t;
+using u32 = uint32_t;
+using i8 = int8_t;
+using u8 = uint8_t;
+using i16 = int16_t;
+using u16 = uint16_t;
+// using uint128_t = __uint128_t;
+
+struct Input {
+  Input(istream &in) : in(&in) {}
+  istream *in;
+  template<class T> T next() const { T x; *in >> x; return x; }
+  int ni() const { return next<int>(); }
+  i64 ni64() const { return next<i64>(); }
+  string ns() const { return next<string>(); }
+  template<class T> vec<T> nvec(int n) const { vec<T> v(n); for (int i = 0; i < n; ++i) v[i] = next<T>(); return v; }
+  vec<int> nvi(int n) const { return nvec<int>(n); }
+  string getline() { string s; std::getline(*in, s); return s; }
+};
+
+struct Output {
+  Output(ostream &out) : out(&out) {}
+  ostream *out;
+  inline void print() {}
+  template<typename T> inline void printOne(const T& v) { *out << v; }
+  template<typename T, size_t N> inline void printOne(const array<T, N>& v) { for (int i = 0; i < N; i++) { if (i) *out << ' '; printOne(v[i]); } }
+  template<typename T> inline void printOne(const vec<T>& v) { for (int i = 0; i < v.size(); i++) { if (i) *out << ' '; printOne(v[i]); } }
+  template<typename T> inline void printOne(const vec2d<T>& v) { for (int i = 0; i < v.size(); i++) println(v[i]); }
+  template<typename A, typename B> inline void printOne(const pair<A, B>& pair) { print(pair.first, pair.second); }
+  template<typename T, typename...Ts> inline void print(const T &f, const Ts&... args) {
+    printOne(f);
+    if (sizeof...(args) != 0) { *out << ' '; print(args...); }
+  }
+  template<typename...Ts> inline void println(const Ts&... args) { print(args...); *out << '\n'; }
+  template<typename...Ts> inline void operator() (const Ts&... args) { println(args...); }
+  void setPrecision(int p) { *out << std::setprecision(p) << std::fixed; }
+};
+
+template<typename T> vec2d<T> newVec2d(int n, int m, const T& init) {
+  vec2d<T> v(n, vec<T>(m, init));
+  return v;
+}
+
+template<typename T> vec3d<T> newVec3d(int n, int m, int k, const T& init) {
+  vec3d<T> v(n, newVec2d(m, k, init));
+  return v;
+}
+
+void panic() {
+  throw 42;
+}
+
+template<typename T> bool rmn(T& value, const T& candidate) {
+  if (candidate < value) { value = candidate; return true; }
+  return false;
+}
+
+template<typename T> bool rmx(T& value, const T& candidate) {
+  if (candidate > value) { value = candidate; return true; }
+  return false;
+}
+
+void solve(Input& in, Output& out);
+int main() {
+  // ifstream fin("input.txt");
+  // Input in(fin);
+  // ofstream fout("output.txt");
+  // Output out(fout);
+  Input in(cin);
+  Output out(cout);
+  solve(in, out);
+  return 0;
+}
+
+void solve(Input& in, Output& out) {
+  string s = in.ns();
+  int n = s.length();
+
+  vector<int> center(n, 3 * n);
+  // http://e-maxx.ru/algo/palindromes_count
+
+  vector<int> d1(n);
+  int l = 0;
+  int r = -1;
+  for (int i = 0; i < n; ++i) {
+    int k = i > r ? 1 : min(d1[l + r - i], r - i + 1);
+    while (i + k < n && i - k >= 0 && s[i + k] == s[i - k])
+      ++k;
+    d1[i] = k;
+    if (i + k - 1 > r) {
+      l = i - k + 1;
+      r = i + k - 1;
+    }
+    if (k > 0)
+      center[i + k - 1] = min(center[i + k - 1], 2 * i);  // i is the center
+  }
+
+  // out("d1", d1);
+
+  vector<int> d2(n);
+  l = 0;
+  r = -1;
+  for (int i = 0; i < n; ++i) {
+    int k = i > r ? 0 : min(d2[l + r - i + 1], r - i + 1);
+    while (i + k < n && i - k - 1 >= 0 && s[i + k] == s[i - k - 1])
+      ++k;
+    d2[i] = k;
+    if (i + k - 1 > r) {
+      l = i - k;
+      r = i + k - 1;
+    }
+    if (k > 0)
+      center[i + k - 1] = min(center[i + k - 1], 2 * i - 1);  // [i-1,i] is the center
+  }
+
+  // out("d2", d2);
+
+  for (int i = n - 2; i >= 0; --i)
+    center[i] = min(center[i], center[i + 1]);
+
+  // out("center", center, "n", n);
+
+  array<u64, 3> p = { 17, 31, 53 };
+  array<u64, 3> mod = { 1100000009, 1200000041, 1150000009 };
+  // out("here we are before");
+
+  vec<array<u64, 3>> h(n + 1, {0, 0, 0});
+  vec<array<u64, 3>> pow(n + 1, {1, 1, 1});
+  // out("here we are");
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < 3; ++j) {
+      h[i + 1][j] = (h[i][j] * p[j] + (s[i] - 'a' + 1)) % mod[j];
+      pow[i + 1][j] = (pow[i][j] * p[j]) % mod[j];
+    }
+  }
+
+  // out("here we are2");
+  set<array<u64, 3>> seen;
+  for (int i = 0; i < n; ++i) {
+    int c = center[i];
+    int left = c % 2 == 0 ? 2 * (c / 2) - i : 2 * ((c + 1) / 2) - i - 1;
+    array<u64, 3> hr = {0, 0, 0};
+    for (int j = 0; j < 3; ++j)
+      hr[j] = (h[i + 1][j] + mod[j] - (h[left][j] * pow[i + 1 - left][j] % mod[j])) % mod[j];
+    // out("hr", hr);
+    seen.insert(hr);
+    if (i > 0)
+      out.print(' ');
+    out.print(seen.size());
+  }
+  out.println();
+}
